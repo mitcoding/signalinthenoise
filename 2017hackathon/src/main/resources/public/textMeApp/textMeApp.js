ad80c7756f204194a6796ca2d63798dc8036acad
Timothy Gross <mitcoding@gmail.com>
2017-05-10 13:46:16 -0700

    finished with angular routs

--- 2017hackathon/src/main/resources/public/textMeApp/textMeApp.js
+++ 2017hackathon/src/main/resources/public/textMeApp/textMeApp.js
@@ -38,33 +38,49 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider.state({
 	    name: 'validation.phone-number',
 	    url: '/phone-number',
-	    controller: ['$state', function($state) {
+	    controller: ['$state', '$scope', function($state, $scope) {
+	    	$scope.currentStateName = $state.current.name + ": "+ ($state.current.name === "validation.phone-number");
+	    	$scope.hasError = false;
+
 	    	if ($state.current.name === "validation.phone-number") {
-	    		$state.transitionTo('validation.phone-number.index');
+	    		$state.go('validation.phone-number.index');
 	    	}
+	    }]
+	});
+
+	$stateProvider.state({
+	    name: 'validation.phone-number.index',
+	    url: '/index',
+	    templateUrl: 'textMeApp/views/validation/phone-number/index.html',
+	    controller: ["$state", "$scope", function($state, $scope) {
+	    	$scope.currentStateName = $scope.currentStateName = $state.current.name + " (index): "+ ($state.current.name === "validation.phone-number");
+	    	$scope.hasError = false;
 	    }]
 	});
 	
 	$stateProvider.state({
-	    name: 'validation.phone-number.index',
-	    url: '/index',
-	    templateUrl: 'textMeApp/views/validation/phone-number/index.html'
-	});
-
-	$stateProvider.state({
 	    name: 'validation.phone-number.success',
 	    url: '/success',
-	    templateUrl: 'textMeApp/views/validation/phone-number/success.html'
+	    templateUrl: 'textMeApp/views/validation/phone-number/success.html',
+	    controller: ["$state", "$scope", function($state, $scope) {
+	    	$scope.currentStateName = $scope.currentStateName = $state.current.name + " (success): "+ ($state.current.name === "validation.phone-number");
+	    	$scope.hasError = false;
+	    }]
 	});
 	
 	$stateProvider.state({
 	    name: 'validation.phone-number.error',
 	    url: '/error',
-	    templateUrl: 'textMeApp/views/validation/phone-number/error.html'
+	    templateUrl: 'textMeApp/views/validation/phone-number/error.html',
+	    controller: ["$state", "$scope", function($state, $scope) {
+	    	$scope.currentStateName = $scope.currentStateName = $state.current.name + " (error): "+ ($state.current.name === "validation.phone-number");
+	    	
+	    	$scope.hasError = true;
+	    }]
 	});
 
 	
-	//$urlRouterProvider.otherwise("/");
+	$urlRouterProvider.otherwise("/");
 });
 
 myApp.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
